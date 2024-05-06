042f025e06e9fcf6b072b1cbe44e1036bce372b1
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2024-05-06 13:45:05 -0700

    Moves data into an array.

--- src/app.rs
+++ src/app.rs
@@ -2,16 +2,59 @@ use yew::prelude::*;
 
 #[function_component]
 pub fn App() -> Html {
-	let world = use_state(|| Exercise::from([
-		"Arms",
-		"Hold a 1 lb weight in each hand. Lie on your back with your arms spread out on both sides like the letter T. The palms of your hands are facing upwards toward the ceiling.",
-		"Take a deep breath, tighten the muscles in your legs while keeping the knees straight, and curl your toes toward your head.",
-		"Raise your arms up and towards each other while keeping the elbows straight. Stop when your hands are straight above you and your arms are pointing towards the ceiling.",
-		"Push your hands up and closer to the ceiling using your shoulders and chest. Stop when your hands are as close to the ceiling as they go.",
-		"Relax your shoulders, then move your arms move back to the floor. When your hands reach the floors, uncurl your toes and breathe out.",
-		"Continue with Exercise 2."
-	]));
-	let exercise = &*world;
+	let world = use_state(|| {
+		[
+			Exercise::from([
+				"Exercise 1",
+				"Arms",
+				"Hold a 1 lb weight in each hand. Lie on your back with your arms spread out on both sides like the letter T. The palms of your hands are facing upwards toward the ceiling.",
+				"Take a deep breath, tighten the muscles in your legs while keeping the knees straight, and curl your toes toward your head.",
+				"Raise your arms up and towards each other while keeping the elbows straight. Stop when your hands are straight above you and your arms are pointing towards the ceiling.",
+				"Push your hands up and closer to the ceiling using your shoulders and chest. Stop when your hands are as close to the ceiling as they go.",
+				"Relax your shoulders, then move your arms move back to the floor. When your hands reach the floors, uncurl your toes and breathe out.",
+				"Continue with Exercise 2."
+			]),
+		]
+	});
+	let exercises = &*world;
+	let headers = exercises.iter().map(|exercise| {
+		html! {
+			<th>
+				<p>{&exercise.name}</p>
+				<p class="tag is-info">{&exercise.target}</p>
+			</th>
+		}
+	}).collect::<Vec<Html>>();
+	let starts = exercises.iter().map(|exercise| {
+		html! {
+			<td>{&exercise.start_position}</td>
+		}
+	}).collect::<Vec<Html>>();
+	let breath_ins = exercises.iter().map(|exercise| {
+		html! {
+			<td>{&exercise.breath_in}</td>
+		}
+	}).collect::<Vec<Html>>();
+	let primaries = exercises.iter().map(|exercise| {
+		html! {
+			<td>{&exercise.primary}</td>
+		}
+	}).collect::<Vec<Html>>();
+	let extras = exercises.iter().map(|exercise| {
+		html! {
+			<td>{&exercise.extra}</td>
+		}
+	}).collect::<Vec<Html>>();
+	let breath_outs = exercises.iter().map(|exercise| {
+		html! {
+			<td>{&exercise.breath_out}</td>
+		}
+	}).collect::<Vec<Html>>();
+	let nexts = exercises.iter().map(|exercise| {
+		html! {
+			<td>{&exercise.next}</td>
+		}
+	}).collect::<Vec<Html>>();
 	html! {
 	<>
 	<section class="section">
@@ -24,38 +67,16 @@ pub fn App() -> Html {
 		<table class="table">
 		<thead>
 		<tr>
-			<th></th>
-			<th>
-				<p>{"Exercise 1"}</p>
-				<p class="tag is-info">{&exercise.target}</p>
-			</th>
+			<th></th>{headers}
 		</tr>
 		</thead>
 		<tbody>
-		<tr>
-			<th>{"Starting Position"}</th>
-			<td>{&exercise.start_position}</td>
-		</tr>
-		<tr>
-			<th>{"Breathe In"}</th>
-			<td>{&exercise.breath_in}</td>
-		</tr>
-		<tr>
-			<th>{"Primary Movement"}</th>
-			<td>{&exercise.primary}</td>
-		</tr>
-		<tr>
-			<th>{"Extra Movement"}</th>
-			<td>{&exercise.extra}</td>
-		</tr>
-		<tr>
-			<th>{"Breathe Out"}</th>
-			<td>{&exercise.breath_out}</td>
-		</tr>
-		<tr>
-			<th>{"Next"}</th>
-			<td>{&exercise.next}</td>
-		</tr>
+		<tr><th>{"Starting Position"}</th>{starts}</tr>
+		<tr><th>{"Breathe In"}</th>{breath_ins}</tr>
+		<tr><th>{"Primary Movement"}</th>{primaries}</tr>
+		<tr><th>{"Extra Movement"}</th>{extras}</tr>
+		<tr><th>{"Breathe Out"}</th>{breath_outs}</tr>
+		<tr><th>{"Next"}</th>{nexts}</tr>
 		</tbody>
 		</table>
 		</p>
@@ -68,6 +89,7 @@ pub fn App() -> Html {
 
 #[derive(Debug, Clone)]
 struct Exercise {
+	pub name: String,
 	pub target: String,
 	pub start_position: String,
 	pub breath_in: String,
@@ -77,16 +99,17 @@ struct Exercise {
 	pub next: String,
 }
 
-impl From<[&'static str; 7]> for Exercise {
-	fn from(value: [&'static str; 7]) -> Self {
+impl From<[&'static str; 8]> for Exercise {
+	fn from(value: [&'static str; 8]) -> Self {
 		Self {
-			target: value[0].to_string(),
-			start_position: value[1].to_string(),
-			breath_in: value[2].to_string(),
-			primary: value[3].to_string(),
-			extra: value[4].to_string(),
-			breath_out: value[5].to_string(),
-			next: value[6].to_string(),
+			name: value[0].to_string(),
+			target: value[1].to_string(),
+			start_position: value[2].to_string(),
+			breath_in: value[3].to_string(),
+			primary: value[4].to_string(),
+			extra: value[5].to_string(),
+			breath_out: value[6].to_string(),
+			next: value[7].to_string(),
 		}
 	}
 }
